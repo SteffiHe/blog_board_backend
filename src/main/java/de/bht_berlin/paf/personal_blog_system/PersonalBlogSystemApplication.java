3a73e6f17c8c4245c890f65e028ad54312e15bfc
Stephanie Heinemann <s50839@bht-berlin.de>
2024-11-20 15:27:19 +0100

    Add .DS_Store to .gitignore

---
+++ src/main/java/de/bht_berlin/paf/personal_blog_system/PersonalBlogSystemApplication.java
@@ -0,0 +1,13 @@
+package de.bht_berlin.paf.personal_blog_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBlogSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBlogSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/bht_berlin/paf/personal_blog_system/PersonalBlogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bht_berlin.paf.personal_blog_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
