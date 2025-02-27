cade9d861f403a1a450c1e3a184f46b3a7346939
yzh <yzh@ivu.de>
2025-02-27 19:53:58 +0100

    Yanbo_BNB23350: delete all files in main branch

--- src/main/java/paf/personal_blog_system/repo/BlogRepo.java
+++
@@ -1,9 +0,0 @@
-package paf.personal_blog_system.repo;
-
-import org.springframework.stereotype.Repository;
-import paf.personal_blog_system.entity.Blog;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-@Repository
-public interface BlogRepo extends MongoRepository<Blog, String> {
-}
--- src/main/java/paf/personal_blog_system/repo/TagRepo.java
+++
@@ -1,9 +0,0 @@
-package paf.personal_blog_system.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import paf.personal_blog_system.entity.Tag;
-
-@Repository
-public interface TagRepo extends MongoRepository<Tag, String> {
-}
