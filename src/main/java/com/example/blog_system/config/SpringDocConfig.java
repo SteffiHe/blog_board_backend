a898d14b9dc4a3dc54a3359eda7d38c9ca786759
yzh <yzh@ivu.de>
2025-02-27 19:45:13 +0100

    BNB23350: add files

---
+++ src/main/java/com/example/blog_system/config/SpringDocConfig.java
@@ -0,0 +1,39 @@
+package com.example.blog_system.config;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.*;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("brand")
+                .pathsToMatch("/brand/**")
+                .build();
+    }
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi.builder()
+                .group("user")
+                .pathsToMatch("/user/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi allApi() {
+        return GroupedOpenApi.builder()
+                .group("all")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+}
+
---
+++ src/main/java/com/example/blog_system/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.blog_system.config;
+
+import com.example.blog_system.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/login", "/register");
+        registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
+    }
+}
