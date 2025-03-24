a48178cbf80cb9f0b819ea827a221f35422948bd
yzh <yzh@ivu.de>
2025-03-24 22:55:10 +0100

    Yanbo_BNB23350: adopt SpringDocConfig

--- src/main/java/com/example/blog_system/config/SpringDocConfig.java
+++ src/main/java/com/example/blog_system/config/SpringDocConfig.java
@@ -13,17 +13,49 @@ import org.springframework.context.annotation.Configuration;
 public class SpringDocConfig {
 
     @Bean
-    public GroupedOpenApi publicApi() {
+    public GroupedOpenApi articleApi() {
         return GroupedOpenApi.builder()
-                .group("brand")
-                .pathsToMatch("/brand/**")
+                .group("article")
+                .pathsToMatch("/api/article/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi categoryApi() {
+        return GroupedOpenApi.builder()
+                .group("category")
+                .pathsToMatch("/api/category/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi rateApi() {
+        return GroupedOpenApi.builder()
+                .group("rate")
+                .pathsToMatch("/api/rate/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi recommendationApi() {
+        return GroupedOpenApi.builder()
+                .group("recommendation")
+                .pathsToMatch("/api/recommendation/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi tagApi() {
+        return GroupedOpenApi.builder()
+                .group("tag")
+                .pathsToMatch("/api/tag/**")
                 .build();
     }
     @Bean
     public GroupedOpenApi userApi() {
         return GroupedOpenApi.builder()
                 .group("user")
-                .pathsToMatch("/user/**")
+                .pathsToMatch("/api/user/**")
                 .build();
     }
 
